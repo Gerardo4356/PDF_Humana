3b11a2e6d232ada6d51b58fb9be51ada977f25f0
Gerardo4356 <gerardocamarillodl@gmail.com>
2022-10-17 11:19:34 -0500

    Agregado el descargar PDF y decodificacion b64url

--- gmail.py
+++ gmail.py
@@ -23,7 +23,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 # import scraping
-
+import base64
+from base64 import urlsafe_b64encode, urlsafe_b64decode
 
 
 def access(printlabels=True):
@@ -72,14 +73,13 @@ def access(printlabels=True):
 def solicitar_constancias(access, driver):
     service = access(False)
     messages = service.users().messages().list(userId='me',q="label:humana is:unread subject:(Servicio Digital: Solicitud de Constancia de Semanas Cotizadas del Asegurado) ").execute()
+    #Obtener id de mensajes
     if len(messages) > 1:    
         id_messages = []
         for message in messages['messages']:
             print(message['id'])
             id_messages.append(message['id']) 
             
-            
-        import base64
         urls_tokens = []
         for i in range(len(id_messages)):
             print("---"+str(i)+"---")
@@ -101,15 +101,45 @@ def solicitar_constancias(access, driver):
         for i in id_messages:
             service.users().messages().modify(userId="me", id=i,body={"removeLabelIds": ['UNREAD']}).execute()
 
-def wdriver():
+def descargar_adjunto(access,driver):
+    service = access(False)
+    messages = service.users().messages().list(userId='me',q="from:(historia.laboral@imss.gob.mx) has:attachment is:unread ").execute()
+    
+    if len(messages) > 1:    
+        id_messages = []
+        for message in messages['messages']:
+            print(message['id'])
+            id_messages.append(message['id']) 
+
+        for i in range(len(id_messages)):
+            correo = service.users().messages().get(userId='me',id=id_messages[i],format='full').execute()
+            attachmentId = correo['payload']['parts'][1]['body']['attachmentId'] 
+            attachment = service.users().messages().attachments().get(userId='me',messageId=id_messages[i],id=attachmentId).execute()['data']
+            # attachment = "b'" + str(attachment) + "'"
+            file = urlsafe_b64decode(attachment)
+
+        with open(r"PDF/test.pdf", "wb") as fh:
+            fh.write(file)
+
+
+def wdriver(headless=True):
     chrome_service = ChromeService(executable_path=ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.add_argument('headless')
+    if headless: options.add_argument('headless')
     driver = webdriver.Chrome(service=chrome_service, options=options)
     return driver
 
+
+
 if __name__ == '__main__':
+    os.system('cls')
+    driver = wdriver()
+    descargar_adjunto(access,driver)
+
+
+
+if __name__ == '__main_':
     os.system('cls')
     print("Cargando SCRAPING Y DRIVER...")
     #region Librerías
